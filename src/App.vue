2e9ee878a9ec1c8cec57ab7efde42eb55adbd70f
muhonja <linahmuhonjaimbari@gmail.com>
2024-10-24 16:14:41 +0300

    initialize vue app

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <div class="container">
+    <h1 class="text-primary flex justify-start">Hello, Bootstrap with Vue and TypeScript!</h1>
+    <button class="btn btn-primary">Click Me</button>
+  </div>
+</template>
+
+<style scoped>
+@import "styles.css";
+
+body {
+  font-family: 'SF Pro Regular', sans-serif, serif,  Ubuntu;
+}
+
+</style>
