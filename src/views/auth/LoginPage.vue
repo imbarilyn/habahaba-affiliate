8e2c95f422c48e17fbc0843b3825ba6a4f99fb05
muhonja <linahmuhonjaimbari@gmail.com>
2024-12-06 17:07:09 +0300

    add alertBanner

--- src/views/auth/LoginPage.vue
+++ src/views/auth/LoginPage.vue
@@ -1,13 +1,16 @@
 <script setup lang="ts">
 import { type AffiliateLoginPayload, useAuthStore } from '@/stores'
-import { computed, reactive, watch } from 'vue'
+import { computed, onMounted, reactive, ref, watch } from 'vue'
 import { useField } from 'vee-validate'
 import { useRouter } from 'vue-router'
-import { bannerAlert, bannerAlertContainer } from '@/alertConfig'
-import BannerToast from '@/components/BannerToast.vue'
+import AlertBanner from '@/components/AlertBanner.vue'
+import { useStorage } from '@vueuse/core'
 
 const authStore = useAuthStore()
 const router = useRouter()
+const loginData = useStorage('formData', {
+  phoneNo: '',
+})
 const affiliatePayload: AffiliateLoginPayload = reactive({
   phoneNo: '',
   password: '',
@@ -28,7 +31,6 @@ const phoneNoValidator = (value: string) => {
   if (value.length > 10 && value.length < 10) {
     return 'Phone number must be 10 digits'
   }
-
   return true
 }
 
@@ -42,6 +44,7 @@ watch(
   () => affiliatePayload.phoneNo,
   value => {
     phoneNo.value = value
+  localStorage.setItem('formData', JSON.stringify({phoneNo: value}))
   },
 )
 
@@ -72,6 +75,12 @@ watch(
   },
 )
 
+const isLoading = ref(false)
+const isAlertBannerVisible = ref({
+  visible: false,
+  message: '',
+  type: '' as 'success' |'danger' | 'warning' | 'primary'
+})
 const everyThingIaValid = computed(() => {
   return (
     phoneNoMeta.validated &&
@@ -82,127 +91,216 @@ const everyThingIaValid = computed(() => {
 })
 const handleLogin = () => {
   if (everyThingIaValid.value) {
+    isLoading.value = true
     authStore
       .loginAffiliate(affiliatePayload)
       .then(resp => {
         console.log(resp)
+        isAlertBannerVisible.value =  {
+          visible: true,
+          message: 'An error occurred',
+          type: 'danger'
+        }
         if (resp.result === 'ok') {
           router.push({
             name: 'dashboard',
           })
         } else {
-          bannerAlert('error', 'An error occurred, please try again')
+          isAlertBannerVisible.value =  {
+            visible: true,
+            message: resp.message,
+            type: 'danger'
+          }
         }
       })
-      .catch((error) => {
+      .catch(error => {
         console.log(error)
-        bannerAlert('error', 'An error occurred, please try again')
+        isAlertBannerVisible.value =  {
+          visible: true,
+          message: 'An error occurred',
+          type: 'danger'
+        }
+      })
+      .finally(()=>{
+        isLoading.value = false
       })
   }
 }
+
+const closeAlert = () =>{
+  console.log('close alert')
+  isAlertBannerVisible.value.visible = false
+}
+
+
+
+//loading cached data for login form
+const loadCachedData = ()=>{
+  const cachedData =  loginData.value
+  console.log(cachedData)
+  if(cachedData){
+    affiliatePayload.phoneNo = cachedData.phoneNo
+  }
+}
+
+
+onMounted(()=>{
+  // On mounted load form data
+  loadCachedData()
+
+
+
+
+})
 </script>
 
 <template>
-  <div
-    id="layoutAuthentication"
-    class="d-flex align-items-center justify-content-center"
-    style="min-height: 100vh"
-  >
-    <div id="layoutAuthentication_content">
-      <main>
-        <div class="container-xl px-4 h-100 col-sm-12 col-md-9">
-          <div class="row justify-content-center">
-            <div class="">
-              <!-- Basic login form-->
-              <div class="card shadow-lg border-0 rounded-lg">
-                <div class="justify-content-center pt-4">
-                  <div class="d-flex justify-content-center pb-3">
-                    <img
-                      src="../../../public/images/habahaba-logo.png"
-                      alt="habahaba-logo"
-                      class="w-25"
-                    />
+  <div class="d-flex md-align-items-center " style="height: 100vh">
+    <div class="row w-100">
+      <div class="col-md col-12 d-flex justify-content-center align-items-center">
+        <div class="">
+          <img
+            src="../../../public/images/referrer-register.png"
+            class="width-size"
+          />
+        </div>
+      </div>
+<!--      <div>-->
+<!--        <img src="../../../public/images/habahaba-logo.png" class="w-25">-->
+<!--      </div>-->
+      <div class="col d-flex justify-content-center align-items-start align-items-md-center ms-4">
+        <div class="card shadow-lg border-0 rounded-lg">
+          <div class="justify-content-center pt-4">
+            <div class="d-flex justify-content-center pb-3">
+              <img
+                src="../../../public/images/habahaba-logo.png"
+                alt="habahaba-logo"
+                class="w-25"
+              />
+            </div>
+            <h3 class="fw-light text-center">Welcome Back!</h3>
+            <p class="fw-light fs-6 text-center">
+              Hey! enter your details to login in to your account
+            </p>
+          </div>
+          <div class="card-body">
+            <!-- Login form-->
+            <form @submit.prevent="handleLogin">
+              <!-- Form Group (email address)-->
+              <div class="mb-md-3">
+                <label class="small mb-1" for="inputUsername"
+                  >Phone Number</label
+                >
+                <input
+                  v-model="affiliatePayload.phoneNo"
+                  class="form-control"
+                  id="inputPhoneNumber"
+                  type="text"
+                  placeholder="0712345678"
+                  style="font-size: 15px"
+                  required
+                  :class="{
+                    'is-invalid': phoneNoMeta.validated && !phoneNoMeta.valid,
+                  }"
+                />
+                <small
+                  v-if="phoneNoMeta.validated && !phoneNoMeta.valid"
+                  class="invalid-feedback"
+                  >{{ phoneNoErrorMessage }}</small
+                >
+              </div>
+              <!-- Form Group (password)-->
+              <div class="mb-md-3">
+                <label class="small mb-1 text-" for="inputPassword"
+                  >Password</label
+                >
+                <input
+                  v-model="affiliatePayload.password"
+                  class="form-control"
+                  id="inputPassword"
+                  type="password"
+                  placeholder="Enter password"
+                  required
+                  style="font-size: 15px"
+                  :class="{
+                    'is-invalid': passwordMeta.validated && !passwordMeta.valid,
+                  }"
+                />
+                <small
+                  v-if="passwordMeta.validated && !passwordMeta.valid"
+                  class="invalid-feedback"
+                  >{{ passwordErrorMessage }}</small
+                >
+                <div class="d-flex justify-content-end pt-2">
+                  <div class="small">
+                    <span>Not a user? </span>
+                    <RouterLink
+                      to="/"
+                      class="text-decoration-none text-habahaba-500"
+                      >Sign up!
+                    </RouterLink>
                   </div>
-                  <h3 class="fw-light text-center">Welcome Back!</h3>
-                  <p class="fw-light fs-6 text-center">
-                    Hey! enter your details to login in to your account
-                  </p>
-                </div>
-                <div class="card-body">
-                  <!-- Login form-->
-                  <form @submit.prevent="handleLogin">
-                    <!-- Form Group (email address)-->
-                    <div class="mb-3">
-                      <label class="small mb-1" for="inputUsername"
-                        >Phone Number</label
-                      >
-                      <input
-                        v-model="affiliatePayload.phoneNo"
-                        class="form-control"
-                        id="inputPhoneNumber"
-                        type="text"
-                        placeholder="0712345678"
-                        style="font-size: 15px"
-                        required
-                        :class="{
-                          'is-invalid':
-                            phoneNoMeta.validated && !phoneNoMeta.valid,
-                        }"
-                      />
-                      <small
-                        v-if="phoneNoMeta.validated && !phoneNoMeta.valid"
-                        class="invalid-feedback"
-                        >{{ phoneNoErrorMessage }}</small
-                      >
-                    </div>
-                    <!-- Form Group (password)-->
-                    <div class="mb-3">
-                      <label class="small mb-1 text-" for="inputPassword"
-                        >Password</label
-                      >
-                      <input
-                        v-model="affiliatePayload.password"
-                        class="form-control"
-                        id="inputPassword"
-                        type="password"
-                        placeholder="Enter password"
-                        required
-                        style="font-size: 15px"
-                        :class="{
-                          'is-invalid':
-                            passwordMeta.validated && !passwordMeta.valid,
-                        }"
-                      />
-                      <small
-                        v-if="passwordMeta.validated && !passwordMeta.valid"
-                        class="invalid-feedback"
-                        >{{ passwordErrorMessage }}</small
-                      >
-                      <div class="d-flex justify-content-end pt-2">
-                        <div class="small">
-                          <span>Not a user? </span>
-                          <RouterLink to="/" class="text-decoration-none"
-                            >Sign up!
-                          </RouterLink>
-                        </div>
-                      </div>
-                    </div>
-
-                    <button
-                      type="submit"
-                      class="btn btn-primary col-12 mt-3 mb-4"
-                    >
-                      login
-                    </button>
-                  </form>
                 </div>
               </div>
-            </div>
+
+              <button
+                type="submit"
+                class="btn btn-habahaba-500 col-12 mt-3 mb-4"
+              >
+                <span v-if="!isLoading" class="text-white">login</span>
+                <span
+                  v-else
+                  class="spinner-border text-habahaba-200"
+                  role="status"
+                >
+                  <span class="visually-hidden">Loading...</span>
+                </span>
+              </button>
+            </form>
           </div>
         </div>
-      </main>
+      </div>
     </div>
+    <Teleport to="body">
+      <div v-if="isAlertBannerVisible.visible">
+        <AlertBanner  :alert-type="isAlertBannerVisible.type" :alert-message="isAlertBannerVisible.message" @close-alert="closeAlert"/>
+      </div>
+    </Teleport>
   </div>
 </template>
 
-<style scoped></style>
+<style scoped>
+.width-size {
+  width: 500px;
+}
+
+
+@media (max-width: 576px) {
+  .width-size {
+    width: 200px;
+  }
+}
+
+@media (min-width:576px) {
+  .width-size {
+    width: 400px;
+  }
+
+}
+
+.form-width {
+  width: 600px;
+}
+
+@media (max-width: 576px) {
+  .form-width {
+    width: 400px;
+  }
+}
+
+@media (min-width: 576px) {
+  .form-width {
+    width: 600px;
+  }
+}
+</style>
