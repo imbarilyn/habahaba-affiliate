5b05d894de5a8a5a3827e08905adc7e9eb3259f0
muhonja <linahmuhonjaimbari@gmail.com>
2024-12-09 16:34:58 +0300

    add TransactionsPage

---
+++ src/components/pages/TransactionsPage.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <div class="container my-4">
+    <div>
+
+    </div>
+
+    <div>
+
+    </div>
+
+  </div>
+
+</template>
+
+<style scoped>
+
+</style>
