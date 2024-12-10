56d27338496eac925344d839bcd1c29f99bbd452
muhonja <linahmuhonjaimbari@gmail.com>
2024-12-10 16:58:50 +0300

    add functionality to validate affiliate link

--- src/stores/affiliateStore.ts
+++ src/stores/affiliateStore.ts
@@ -44,6 +44,95 @@ export const useAffiliateStore =  defineStore('useAffiliateStore', ()=>{
         console.log('error occurred')
         return
       }
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  async function validateAffiliateLink(affiliateCode: string) {
+    try {
+      const response = await fetch(`${BASE_URL}/affiliate/validate-link/`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        mode: 'cors',
+        body: JSON.stringify({
+          code: affiliateCode
+        })
+      })
+      return await response.json()
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  async function affiliateUserPhone(affiliateUserPayload: AffiliateUserPayload) {
+    console.log(affiliateUserPayload)
+    try {
+      const response = await fetch(`${BASE_URL}/affiliate-user/submit-phone/`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        mode: 'cors',
+        body: JSON.stringify(affiliateUserPayload)
+      })
+      const resp = await response.json()
+      console.log(resp)
+      return resp
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  async function getDashboardData(token: string) {
+    try {
+      const response = await fetch(`${BASE_URL}/affiliate/dashboard-data/`, {
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization: `${token}`
+        },
+        mode: 'cors'
+      })
+
+      const resp = await response.json()
+      // console.log(resp)
+      return resp
+
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  async function getStatistics(duration: string) {
+    const authStore = useAuthStore()
+    try {
+      const response = await fetch(`${BASE_URL}/affiliate/dashboard-data/user-vs-affiliate-earnings/?duration=${duration}`, {
+        method: 'GET',
+        headers: {
+          Authorization: `${authStore.token}`,
+          'Content-Type': 'application/json'
+        },
+        mode: 'cors'
+      })
+      if (!response.ok) {
+        return {
+          result: false,
+          data: null
+        }
+      } else {
+        const stats = await response.json()
+        console.log(stats)
+
+        return {
+          result: true,
+          data: {
+            ...stats.data
+          }
+        }
+      }
     }
     catch(error){
       console.log(error)
